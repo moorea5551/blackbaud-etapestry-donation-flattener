85a151156c82e6ef040f2f0afce40e7266f00571
Andrew Moore <moorea5551@gmail.com>
2023-01-14 14:08:42 -0600

    accept file name arg and parse csv

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+import sys
+import csv
 import Donor
 
 def main():
-    print("hello world")
+    fileName = sys.argv[1]
+    if not fileName:
+        print("Expecting csv file path as first argument")
+        exit()
+
+    with open(fileName, newline='') as csvFile:
+        reader = csv.reader(csvFile, delimiter=',')
+        for row in reader:
+            print(row)
 
 if __name__ == "__main__":
     main()
